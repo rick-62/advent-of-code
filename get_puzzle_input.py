cd061a6a583e61bc7553803b3242101356407849
rick-62 <rick-62@hotmail.co.uk>
2023-07-09 19:08:57 +0000

    init

---
+++ get_puzzle_input.py
@@ -0,0 +1,30 @@
+import requests
+import os
+import sys
+
+
+def download_puzzle_input(day, year):
+    '''download the users puzzle input from Advent of Code site'''
+
+    cookies = {'session': os.getenv('session')}
+
+    r = requests.get(f'https://adventofcode.com/{year}/day/{day}/input', cookies=cookies)
+
+    r.raise_for_status()
+
+    return r
+
+
+def save_puzzle_input(text, day, year):
+    '''save puzzle input as text file'''
+
+    with open(f'advent_of_code_{year}/inputs/day{day:0>2}.txt', 'w') as f:
+        f.write(text)
+    
+
+if __name__ == '__main__':
+
+    day = int(sys.argv[1])
+    year = int(sys.argv[2])
+    r = download_puzzle_input(day, year)
+    save_puzzle_input(r.text, day, year)
