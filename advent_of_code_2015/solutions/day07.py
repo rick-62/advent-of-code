97a60ed0fc1f71e79ef2effd4927217365087ab2
rick-62 <rick-62@hotmail.co.uk>
2023-07-18 20:53:32 +0000

    2015 day07 part 1 in progress

--- advent_of_code_2015/solutions/day07.py
+++ advent_of_code_2015/solutions/day07.py
@@ -3,83 +3,27 @@ import operator
 import re
 from collections import namedtuple
 from dataclasses import dataclass
+from functools import cache
 
 from helper import coalesce, load_input
 
-# Needs restructure as not working as implied:
-    # Need to convert instructions to dictoinary
-    # and then recurse through values etc in order to get desired values
 
-class Operation:
-
-    def __init__(self):
-        self._attrs = {}
-        self._lookup: dict={
-            'AND': operator.and_,
-            'OR': operator.or_,
-            'LSHIFT': operator.lshift,
-            'RSHIFT': operator.rshift,
-            'NOT': operator.invert,
-            'ASSIGN': lambda X: X,
-        }
-        
-    def __getattr__(self, name):
-        if name.startswith("_"):
-            super().__getattr__(name)
-        if name in self._attrs:
-            return self._attrs[name].value
-        else:
-            return int(name)
-
-    def __setattr__(self, name, value):
-        if name.startswith("_"):
-            super().__setattr__(name, value)
-        else:
-            self._attrs[name] = ctypes.c_uint16(value)
-
-    # def _resolve(self, a):
-    #     '''
-    #     Given argument a, which could be an attribute or number, 
-    #     returns either the associated attribute or 
-    #     returns integer representation of number
-    #     '''
-    #     try:
-    #         return int(a)
-    #     except ValueError:
-    #         return getattr(self, a)
-    #     except TypeError:
-    #         return getattr(self, a)
-    
-    def apply(self, operation: namedtuple):
-        if operation.operator in ['ASSIGN', 'NOT']:
-            setattr(
-                self, 
-                operation.Z, 
-                self._lookup[operation.operator](
-                    getattr(self, operation.X)
-                )
-            )
-        else:
-            setattr(
-                self, 
-                operation.Z, 
-                self._lookup[operation.operator](
-                    getattr(self, operation.X),
-                    getattr(self, operation.Y),
-                )
-            )
-        
+LOOKUP = {
+    'AND': operator.and_,
+    'OR': operator.or_,
+    'LSHIFT': operator.lshift,
+    'RSHIFT': operator.rshift,
+    'NOT': operator.invert,
+    'ASSIGN': lambda Z: Z,
+}      
 
 
 def create_input():
     '''
     read in puzzle input from file, 
     using regex to extract coordinates & instruction for each line
-    and returning a list of intructions/coordinates stored as named tuples
+    and returning a dictionary with list of input intructions/coordinates
     '''
-    Operation = namedtuple("Operation", ["operator", "X", "Y", "Z"])
-    operations = []
-
     pattern = re.compile(r"""
 
         # first arguments either number, variable or NOT operator
@@ -93,18 +37,40 @@ def create_input():
 
     """, re.VERBOSE)
 
+    operations = {}
     for line in load_input(day=7).readlines():
         match = re.match(pattern, line)
         if match:
             X1, op1, X2, op2, Y1, Z = match.groups()
-            operations.append(Operation(
-                operator = coalesce(op1, op2, 'ASSIGN'),
-                X = coalesce(X1, X2),
-                Y = Y1,
-                Z = Z
-            ))
+            operations[Z] = [
+                coalesce(op1, op2, 'ASSIGN'),
+                coalesce(X1, X2),
+                Y1
+            ]
     
     return operations
+
+
+@cache
+def resolve(wire):
+
+    if isinstance(wire, int):
+        return wire
+    
+    if wire.isnumeric():
+        return int(wire)   
+
+    op, X, Y = operations[wire]
+    print(op)
+    if op in ['NOT', 'ASSIGN']:
+        return LOOKUP[op](resolve(X))
+    else:
+        return LOOKUP[op](resolve(X), resolve(Y))
+
+    
+
+    
+
 
 def part1():
     '''given the input operations, returns the signal ultimately provided to wire a'''
@@ -112,6 +78,8 @@ def part1():
     for operation in create_input():
         op.apply(operation)
     return op.a
+
+    # ctypes.c_uint16(Z)
     
 def part2():
     ''' '''
--- advent_of_code_2015/tests/test_day07.py
+++ advent_of_code_2015/tests/test_day07.py
@@ -5,37 +5,13 @@ import pytest
 from solutions import day07
 
 
-class TestOperation:
-
-    def test_init(self):
-        op = day07.Operation()
-        assert op._lookup['AND'](1,1) == 1
-        assert op._lookup['OR'](0,1) == 1
-        assert op._lookup['LSHIFT'](1,1) == 2
-        assert op._lookup['NOT'](1) == -2
-        assert op._lookup['ASSIGN'](10) == 10
-
-    # def test__resolve(self):
-    #     op = day07.Operation()
-    #     op.a = 3
-    #     assert op._resolve(2) == 2
-    #     assert op._resolve('2') == 2
-    #     assert op._resolve('a') == 3
-
-    def test_apply(self):
-        op = day07.Operation()
-        Operation = namedtuple("Operation", ["operator", "X", "Y", "Z"])
-
-        op.apply(Operation(operator='ASSIGN', X='123', Y=None, Z='x'))
-        assert op.x == 123
-
-        op.apply(Operation(operator='NOT', X='x', Y=None, Z='h'))
-        assert op.x == 123
-        assert op.h == 65412
-
-        op.apply(Operation(operator='AND', X='x', Y='456', Z='d'))
-        assert op.x == 123
-        assert op.d == 72
+def test_lookup():
+    assert day07.LOOKUP['AND'](1,1) == 1
+    assert day07.LOOKUP['OR'](0,1) == 1
+    assert day07.LOOKUP['LSHIFT'](1,1) == 2
+    assert day07.LOOKUP['NOT'](1) == -2
+    assert day07.LOOKUP['NOT'](66000) == -66001
+    assert day07.LOOKUP['ASSIGN'](10) == 10
 
 
 @pytest.mark.parametrize(
@@ -53,12 +29,42 @@ class TestOperation:
 def test_create_input(s, op, X, Y, Z):
     with patch('builtins.open', mock_open(read_data=s)):
         returns = day07.create_input()
-        assert returns[0].X == X
-        assert returns[0].Y == Y
-        assert returns[0].Z == Z
-        assert returns[0].operator == op
+        assert returns[Z] == [op, X, Y]
+
+
+def test_resolve():
+
+    assert day07.resolve(123) == 123
+    assert day07.resolve('123') == 123
+
+    # basic operation
+    day07.operations={
+        'a': ['AND', 1, 2]
+    }
+    assert day07.resolve('a') == 0
+
+    # recurse
+    day07.operations={
+        'a': ['AND', '1', 'b'],
+        'b': ['LSHIFT', '1', '1'],
+    }
+    assert day07.resolve('a') == 0
+
+    # NOT operation - perk of pytest causing issues
+    del day07.operations
+    day07.operations={
+        'a': ['NOT', '7', None],
+    }
+    assert day07.resolve('a') == -8
+
+    # AND/LSHIFT/ASSIGN/NOT operations
+    day07.operations={
+        'a': ['AND', '1', 'b'],
+        'b': ['LSHIFT', 'c', '1'],
+        'c': ['ASSIGN', 'd', None],
+        'd': ['NOT', '66000', None]
+    }
+    assert day07.resolve('a') == 0
+    assert day07.resolve('b') == -132002
 
     
-
-
-
