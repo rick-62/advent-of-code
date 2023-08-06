d335d28eb14f70300dc767b87b99dcde03b5dacb
rick-62 <rick-62@hotmail.co.uk>
2023-08-06 08:05:54 +0000

    2015 day15 butchered attempt

--- advent_of_code_2015/solutions/day15.py
+++ advent_of_code_2015/solutions/day15.py
@@ -1,6 +1,6 @@
 import re
 from collections import defaultdict
-from itertools import permutations
+from itertools import product, starmap
 from typing import Dict, List, Tuple
 
 from helper import load_input
@@ -37,11 +37,104 @@ def parse_input(lines: List[str]) -> Dict[str, Dict]:
                 if v.isdigit() or v.startswith("-")
             }
 
-    return ingredients
+    # swap ingredients and properties to create alternative view
+    properties = {}
+    for k, v in ingredients.items():
+        for k2, v2 in v.items():
+            properties.setdefault(k2, {})[k] = v2
+
+    return ingredients, properties
 
 
 def part1():
-    ...    
+
+    ingredients, properties = parse_input(create_input())
+
+    # preset ingredient ranges
+    valid_teaspoons = {i: set(range(1, 98)) for i in ingredients}
+
+    scores = defaultdict(set)
+
+    # extract each property at a time
+    for prop, v in properties.items():
+
+        if prop == 'calories':
+            continue
+    
+        code = ''
+        active = {}
+        for ingredient, prop_per_teaspoon in v.items():
+
+            if prop_per_teaspoon == 0:
+                continue
+
+            else:
+                active[ingredient] = 0  # placeholder
+                code += f'+{prop_per_teaspoon}*active["{ingredient}"]'
+            
+        code = compile(code, f'<eval-{prop}>', 'eval')
+
+        list_of_active = list(active.keys())
+
+        orig_valid_teaspoons = valid_teaspoons.copy()
+
+        for ingredient in list_of_active:
+            valid_teaspoons[ingredient] = set([1])
+
+        for prod in product(*[orig_valid_teaspoons[s] for s in list_of_active]):
+
+            if sum(prod) > 100:
+                continue
+
+            for i, ingredient in enumerate(list_of_active):
+                active[ingredient] = prod[i]
+
+            eval_result = eval(code)
+            if eval_result > 0:
+                for ingredient in list_of_active:
+                    valid_teaspoons[ingredient].add(active[ingredient])
+                scores[prop].add(eval_result)
+
+
+    # find maximum
+
+    code = []
+    active = {}
+
+    for prop, v in properties.items():
+
+        active_code = ''
+
+        if prop == 'calories':
+            continue
+    
+        for ingredient, prop_per_teaspoon in v.items():
+
+            active[ingredient] = 0  # placeholder
+
+            if prop_per_teaspoon == 0:
+                continue
+
+            else:
+                active_code += f'+{prop_per_teaspoon}*active["{ingredient}"]'
+        
+        code.append(f'({active_code})')
+
+    code = compile('*'.join(code), f'<eval-all>', 'eval')
+
+    mx = 1
+    for prod in product(*[valid_teaspoons[s] for s in valid_teaspoons.keys()]):
+        if sum(prod) > 100:
+            continue
+
+        for i, ingredient in enumerate(valid_teaspoons.keys()):
+            active[ingredient] = prod[i]
+
+        eval_result = eval(code)
+        if eval_result > mx:
+            mx = eval_result
+
+    return mx
 
 
 def part2():
--- advent_of_code_2015/tests/test_day15.py
+++ advent_of_code_2015/tests/test_day15.py
@@ -47,7 +47,30 @@ def test_create_input(example_input):
     ]
 )
 def test_parse_input(example_input, expected_output):
-    output = day15.parse_input(example_input)
+    output, _ = day15.parse_input(example_input)
     assert output == expected_output
+
+
+@pytest.mark.parametrize(
+    'example_input, expected_output', [
+        (
+            [
+                'Butterscotch: capacity -1, durability -2, flavor 6, texture 3, calories 8', 
+                'Cinnamon: capacity 2, durability 3, flavor -2, texture -1, calories 3'
+            ],
+            {
+                'capacity': {'Butterscotch': -1, 'Cinnamon': 2 },
+                'durability': {'Butterscotch': -2, 'Cinnamon': 3 },
+                'flavor': {'Butterscotch': 6, 'Cinnamon': -2 },
+                'texture': {'Butterscotch': 3, 'Cinnamon': -1 }, 
+                'calories': {'Butterscotch': 8, 'Cinnamon': 3 },
+            },
+    
+        )
+    ]
+)
+def test_parse_input_properties(example_input, expected_output):
+    _, properties = day15.parse_input(example_input)
+    assert properties == expected_output
 
 
