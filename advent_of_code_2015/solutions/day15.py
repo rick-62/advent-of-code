4c39807ea0720078ed2d65d8c05acbf89b0975f1
rick-62 <rick-62@hotmail.co.uk>
2023-08-21 05:12:32 +0000

    2015 day15 complete

--- advent_of_code_2015/solutions/day15.py
+++ advent_of_code_2015/solutions/day15.py
@@ -1,8 +1,8 @@
 import re
 from collections import defaultdict
-from itertools import product, starmap
 from typing import Dict, List, Tuple
 
+import numpy as np
 from helper import load_input
 
 
@@ -13,132 +13,71 @@ def create_input():
 
 def parse_input(lines: List[str]) -> Dict[str, Dict]:
     '''
-    Extracts ingredient names and properties from input (lines), 
-    storing results in a combined dictionary
+    Extracts properties from input (lines), returning 2D numpy array.
+    Each row is in relation to each property. 
+    Each column is in relation to each ingredient.
+    Index of properties/ingredients matches order they appear in input.
     ''' 
-    pattern = re.compile(
-        r'(?P<name>\w+).+?'
-        r'(?P<capacity>-?\d+).+?'
-        r'(?P<durability>-?\d+).+?'
-        r'(?P<flavor>-?\d+).+?'
-        r'(?P<texture>-?\d+).+?'
-        r'(?P<calories>-?\d+)'
-    )
-
-    ingredients = {}
+    properties = []
 
     for line in lines:
+        values = re.findall(r'-?\d+', line)
+        properties.append([int(v) for v in values])
 
-        match = re.match(pattern, line)
+    return np.vstack(properties).T
 
-        if match:
-            ingredients[match.group('name')] = {
-                k: int(v) for k, v in match.groupdict().items() 
-                if v.isdigit() or v.startswith("-")
-            }
 
-    # swap ingredients and properties to create alternative view
-    properties = {}
-    for k, v in ingredients.items():
-        for k2, v2 in v.items():
-            properties.setdefault(k2, {})[k] = v2
+def yield_teaspoon_combinations_for_four_ingredients(max_teaspoons=100):
+    '''loop through possible combinations whose sum does not exceed max_teaspoons'''
+    for i in range(max_teaspoons + 1):
+        for j in range(max_teaspoons + 1 - i):
+            for k in range(max_teaspoons + 1 - i - j):
+                l = max_teaspoons - i - j - k
+                yield np.array([i, j, k, l])
 
-    return ingredients, properties
+
+def calculate_score(teaspoons, properties):
+    '''given teaspoon combination, calculates the score'''
+    arr1 = np.matmul(properties, teaspoons)
+    arr2 = np.where(arr1 < 0, 0, arr1)
+    arr3 = np.prod(arr2)
+    return arr3
 
 
 def part1():
+    '''calculate the max score, excluding calories property'''
 
-    ingredients, properties = parse_input(create_input())
+    # parse input
+    properties = parse_input(create_input())
 
-    # preset ingredient ranges
-    valid_teaspoons = {i: set(range(1, 98)) for i in ingredients}
+    # remove last row (calories)
+    properties = properties[:-1]
 
-    scores = defaultdict(set)
-
-    # extract each property at a time
-    for prop, v in properties.items():
-
-        if prop == 'calories':
-            continue
-    
-        code = ''
-        active = {}
-        for ingredient, prop_per_teaspoon in v.items():
-
-            if prop_per_teaspoon == 0:
-                continue
-
-            else:
-                active[ingredient] = 0  # placeholder
-                code += f'+{prop_per_teaspoon}*active["{ingredient}"]'
-            
-        code = compile(code, f'<eval-{prop}>', 'eval')
-
-        list_of_active = list(active.keys())
-
-        orig_valid_teaspoons = valid_teaspoons.copy()
-
-        for ingredient in list_of_active:
-            valid_teaspoons[ingredient] = set([1])
-
-        for prod in product(*[orig_valid_teaspoons[s] for s in list_of_active]):
-
-            if sum(prod) > 100:
-                continue
-
-            for i, ingredient in enumerate(list_of_active):
-                active[ingredient] = prod[i]
-
-            eval_result = eval(code)
-            if eval_result > 0:
-                for ingredient in list_of_active:
-                    valid_teaspoons[ingredient].add(active[ingredient])
-                scores[prop].add(eval_result)
-
-
-    # find maximum
-
-    code = []
-    active = {}
-
-    for prop, v in properties.items():
-
-        active_code = ''
-
-        if prop == 'calories':
-            continue
-    
-        for ingredient, prop_per_teaspoon in v.items():
-
-            active[ingredient] = 0  # placeholder
-
-            if prop_per_teaspoon == 0:
-                continue
-
-            else:
-                active_code += f'+{prop_per_teaspoon}*active["{ingredient}"]'
-        
-        code.append(f'({active_code})')
-
-    code = compile('*'.join(code), f'<eval-all>', 'eval')
-
-    mx = 1
-    for prod in product(*[valid_teaspoons[s] for s in valid_teaspoons.keys()]):
-        if sum(prod) > 100:
-            continue
-
-        for i, ingredient in enumerate(valid_teaspoons.keys()):
-            active[ingredient] = prod[i]
-
-        eval_result = eval(code)
-        if eval_result > mx:
-            mx = eval_result
-
-    return mx
+    # calculate max score
+    return max([
+        calculate_score(teaspoons, properties) 
+        for teaspoons in  yield_teaspoon_combinations_for_four_ingredients()
+    ])
 
 
 def part2():
-    ...
+    '''calculate the max score, where calorie sum equals 500'''
+
+    # parse input
+    properties = parse_input(create_input())
+
+    # extract calories
+    calories = properties[-1]
+
+    # remove last row
+    properties = properties[:-1]
+
+    # calculate max score, excluding where calories score != 500
+    return max([
+        calculate_score(teaspoons, properties) 
+        for teaspoons in  yield_teaspoon_combinations_for_four_ingredients()
+        if calculate_score(teaspoons, calories) == 500
+    ])
 
 
 if __name__ == '__main__':
--- advent_of_code_2015/tests/test_day15.py
+++ advent_of_code_2015/tests/test_day15.py
@@ -1,5 +1,6 @@
 from unittest.mock import mock_open, patch
 
+import numpy as np
 import pytest
 import pytest_timeout
 from solutions import day15
@@ -20,57 +21,49 @@ def test_create_input(example_input):
 
 
 @pytest.mark.parametrize(
-    'example_input, expected_output', [
-        (
+    'example_input, expected_output', [(
             [
                 'Butterscotch: capacity -1, durability -2, flavor 6, texture 3, calories 8', 
                 'Cinnamon: capacity 2, durability 3, flavor -2, texture -1, calories 3'
             ],
-            {
-                'Butterscotch': {
-                    'capacity': -1, 
-                    'durability': -2, 
-                    'flavor': 6, 
-                    'texture': 3, 
-                    'calories': 8
-                },
-                'Cinnamon': {
-                    'capacity': 2, 
-                    'durability': 3, 
-                    'flavor': -2, 
-                    'texture': -1, 
-                    'calories': 3
-                },
-            },
-    
-        )
-    ]
+            np.array([
+                [-1, 2],
+                [-2, 3],
+                [6, -2],
+                [3, -1],
+                [8, 3],
+            ])
+            ,
+        )]
 )
-def test_parse_input(example_input, expected_output):
-    output, _ = day15.parse_input(example_input)
-    assert output == expected_output
+def test_parse_input_properties(example_input, expected_output):
+    properties = day15.parse_input(example_input)
+    assert np.array_equal(properties, expected_output)
+
+
+def test_yield_teaspoon_combinations_for_four_ingredients():
+    combination = day15.yield_teaspoon_combinations_for_four_ingredients()
+    assert sum(next(combination)) == 100
+    assert np.array_equal(next(combination), np.array([0,0,1,99]))
+
+
+@pytest.fixture()
+def properties1():
+    return (
+        np.array([
+            [-1, 2],
+            [-2, 3],
+            [6, -2],
+            [3, -1],
+        ])
+    )
 
 
 @pytest.mark.parametrize(
-    'example_input, expected_output', [
-        (
-            [
-                'Butterscotch: capacity -1, durability -2, flavor 6, texture 3, calories 8', 
-                'Cinnamon: capacity 2, durability 3, flavor -2, texture -1, calories 3'
-            ],
-            {
-                'capacity': {'Butterscotch': -1, 'Cinnamon': 2 },
-                'durability': {'Butterscotch': -2, 'Cinnamon': 3 },
-                'flavor': {'Butterscotch': 6, 'Cinnamon': -2 },
-                'texture': {'Butterscotch': 3, 'Cinnamon': -1 }, 
-                'calories': {'Butterscotch': 8, 'Cinnamon': 3 },
-            },
-    
-        )
-    ]
+    'example_teaspoons, expected_output', [
+        (np.array([44,56]), 62842880),
+        ]
 )
-def test_parse_input_properties(example_input, expected_output):
-    _, properties = day15.parse_input(example_input)
-    assert properties == expected_output
-
-
+def test_calculate_score(example_teaspoons, expected_output, properties1):
+    score = day15.calculate_score(example_teaspoons, properties1)
+    assert np.array_equal(score, expected_output)
